c1382516a9e482d8c48ac75be5fdd84a0696702a
Dide Aydogan <dide1992@gmail.com>
2023-03-09 10:28:51 -0500

    set up middleware and res.send to index page

---
+++ controllers/foods.js
@@ -0,0 +1,15 @@
+// const express = require('express')
+// const router = express.Router()
+// // const Food = require ('../models/food.js')
+
+//  //Routes
+//  //Index
+//  router.get('/', (req, res) => {
+//     res.send('Hello world')
+//  })
+
+
+
+
+
+// module.exports = router
---
+++ server.js
@@ -0,0 +1,30 @@
+
+const express= require('express')
+const app = express()
+// const methodOverride = require('method-override')
+
+const PORT = process.env.PORT || 9009
+require('dotenv').config();
+// const Food = require('./models/food.js')
+// const foodsController = require('./controllers/foods.js')
+
+
+//Middleware
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+// app.use(methodOverride('_method'))
+app.use(express.static('public'))
+// app.use('/dideskitchen', foodsController)
+
+
+//Routes
+ //Index
+ app.get('/dideskitchen', (req, res) => {
+    res.send('hello world')
+ })
+
+
+//port
+app.listen(PORT, () => {
+    console.log('Server is listening on: '+ PORT)
+})
