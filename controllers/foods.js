8b811b66ce58c86686a0057da98f0a5038401629
Dide Aydogan <dide1992@gmail.com>
2023-03-13 11:18:48 -0400

    my recipe outline added and index page navigates to the right category

--- controllers/foods.js
+++ controllers/foods.js
@@ -18,8 +18,8 @@ router.get('/about', (req, res) => {
 })
 
 //My Recipe Page
-router.get('/myrecipe', (req, res) => {
-   res.render('myRecipe.ejs')
+router.get('/myrecipes', (req, res) => {
+   res.render('myRecipes.ejs')
 })
 
 
@@ -29,30 +29,30 @@ router.get('/new', (req, res) => {
 })
 
 
-router.get('/seed', (req, res) => {
-   Food.create([
-       {
-       recipeName:'Olive Bread',
-       image:url("../img/bake.png"),
-       ingredients: ['600g Flour', '300ml Warm Water', '1 Teaspoon Salt', '1 Instant Yeast','Olives'],
-       directions:'Mix all the ingredients. Cover the dough and let it rise for 3 hours. After 3 hours, start folding the dough for about 10 times and place it on a parchment paper. Cover it again and wait 1 hour. Preheat the oven to 230C/450F and place the dopugh inside the dutch oven and bake it for 30 minutes with the lid on. After 30 minutes, take the lid and the parchement paper off and bake it for another 10 minutes. Bon Appetit!'
-       },
-       {
-         recipeName:'Olive Bread',
-         image: url("../img/bake.png"),
-         ingredients: ['600g Flour', '300ml Warm Water', '1 Teaspoon Salt', '1 Instant Yeast','Olives'],
-         directions:'Mix all the ingredients. Cover the dough and let it rise for 3 hours. After 3 hours, start folding the dough for about 10 times and place it on a parchment paper. Cover it again and wait 1 hour. Preheat the oven to 230C/450F and place the dopugh inside the dutch oven and bake it for 30 minutes with the lid on. After 30 minutes, take the lid and the parchement paper off and bake it for another 10 minutes. Bon Appetit!'
-         },
-       {
-         recipeName:'Olive Bread',
-         image: url("../img/bake.png"),
-         ingredients: ['600g Flour', '300ml Warm Water', '1 Teaspoon Salt', '1 Instant Yeast','Olives'],
-         directions:'Mix all the ingredients. Cover the dough and let it rise for 3 hours. After 3 hours, start folding the dough for about 10 times and place it on a parchment paper. Cover it again and wait 1 hour. Preheat the oven to 230C/450F and place the dopugh inside the dutch oven and bake it for 30 minutes with the lid on. After 30 minutes, take the lid and the parchement paper off and bake it for another 10 minutes. Bon Appetit!'
-         }
-   ], (err, data) => {
-       res.redirect('/didekitchen/myrecipes')
-   })
-})
+// router.get('/seed', (req, res) => {
+//    Food.create([
+//        {
+//        recipeName:'Olive Bread',
+//        image:url("../img/bake.png"),
+//        ingredients: ['600g Flour', '300ml Warm Water', '1 Teaspoon Salt', '1 Instant Yeast','Olives'],
+//        directions:'Mix all the ingredients. Cover the dough and let it rise for 3 hours. After 3 hours, start folding the dough for about 10 times and place it on a parchment paper. Cover it again and wait 1 hour. Preheat the oven to 230C/450F and place the dopugh inside the dutch oven and bake it for 30 minutes with the lid on. After 30 minutes, take the lid and the parchement paper off and bake it for another 10 minutes. Bon Appetit!'
+//        },
+//        {
+//          recipeName:'Olive Bread',
+//          image: url("../img/bake.png"),
+//          ingredients: ['600g Flour', '300ml Warm Water', '1 Teaspoon Salt', '1 Instant Yeast','Olives'],
+//          directions:'Mix all the ingredients. Cover the dough and let it rise for 3 hours. After 3 hours, start folding the dough for about 10 times and place it on a parchment paper. Cover it again and wait 1 hour. Preheat the oven to 230C/450F and place the dopugh inside the dutch oven and bake it for 30 minutes with the lid on. After 30 minutes, take the lid and the parchement paper off and bake it for another 10 minutes. Bon Appetit!'
+//          },
+//        {
+//          recipeName:'Olive Bread',
+//          image: url("../img/bake.png"),
+//          ingredients: ['600g Flour', '300ml Warm Water', '1 Teaspoon Salt', '1 Instant Yeast','Olives'],
+//          directions:'Mix all the ingredients. Cover the dough and let it rise for 3 hours. After 3 hours, start folding the dough for about 10 times and place it on a parchment paper. Cover it again and wait 1 hour. Preheat the oven to 230C/450F and place the dopugh inside the dutch oven and bake it for 30 minutes with the lid on. After 30 minutes, take the lid and the parchement paper off and bake it for another 10 minutes. Bon Appetit!'
+//          }
+//    ], (err, data) => {
+//        res.redirect('/didekitchen/myrecipes')
+//    })
+// })
 //Create new item
 router.post('/', (req, res) => {
   Food.create(req.body, (error, createdFood) => {
